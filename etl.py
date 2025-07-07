23d9126c413a3aa83798c4e3d562babc39ef62bd
Gabe Pineda <gabepineda6@gmail.com>
2025-07-07 12:31:50 -0700

    defined a function to load the raw data csv into a pandas data frame

--- etl.py
+++ etl.py
@@ -3,6 +3,16 @@ import numpy as np
 import os
 
 #Define the data paths
-
 raw_data = os.path.join("..", "data", "raw-data", "listings.csv")
 processed_data = os.path.join("..", "data", "cleaned-data", "listings_cleaned.csv")
+
+#load the raw data into a pandas dataframe
+def load_data():
+
+    print(f"Loading data from {raw_data}...")
+
+    df = pd.read_csv(raw_data)
+
+    print(f"Loaded {len(df)} rows and {len(df.columns)} columns.")
+    return df
+
