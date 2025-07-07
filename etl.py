67fe431deaed0b0478318e7b9c207f9bd0d29dd2
Gabe Pineda <gabepineda6@gmail.com>
2025-07-07 12:28:17 -0700

    created a file for the etl process and defined my data paths

---
+++ etl.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import os
+
+#Define the data paths
+
+raw_data = os.path.join("..", "data", "raw-data", "listings.csv")
+processed_data = os.path.join("..", "data", "cleaned-data", "listings_cleaned.csv")
