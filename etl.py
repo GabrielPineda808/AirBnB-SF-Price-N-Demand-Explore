5f0151a25171af6ed4985130ab10104fbce2a7c4
Gabe Pineda <gabepineda6@gmail.com>
2025-07-07 13:20:22 -0700

    new cleansed data

--- etl.py
+++ etl.py
@@ -3,31 +3,31 @@ import numpy as np
 import os
 
 #Define the data paths
-raw_data = os.path.join("..", "data", "raw-data", "listings.csv")
-processed_data = os.path.join("..", "data", "cleaned-data", "listings_cleaned.csv")
+raw_data = os.path.join("data", "raw-data", "listings.csv")
+processed_data = os.path.join("data", "cleaned-data", "listings_cleaned.csv")
 
 #load the raw data into a pandas dataframe
-def load_data():
+def load_data(file_path):
 
-    print(f"Loading data from {raw_data}...")
+    print(f"Loading data from {file_path}...")
 
-    df = pd.read_csv(raw_data)
+    df = pd.read_csv(file_path)
 
     print(f"Loaded {len(df)} rows and {len(df.columns)} columns.")
     return df
 
 def clean_data(df):
-    print(f"Cleaning data from {raw_data}...")
+    print("Cleaning data...")
 
     #keeping columns that are relevant
-    columns = [
-        "id", "name", "host_id", "neighborhood_cleansed", "room_type",
-        "price", "minimum_nights", "number_of_reviews", "availabilitiy_365",
+    relevant_columns = [
+        "id", "name", "host_id", "neighbourhood_cleansed", "room_type",
+        "price", "minimum_nights", "number_of_reviews", "availability_365",
         "bedrooms", "bathrooms", "review_scores_rating"
     ]
 
     #reassignging data frame to only contain data from relevant columns
-    df = df[columns]
+    df = df[relevant_columns]
 
     #remove rows with missing prices
     df = df[df["price"].notna()]
@@ -35,7 +35,7 @@ def clean_data(df):
     #convert price from a string to a float
     df["price"] = (
         df["price"]
-        .replace('[\$,]', '', regex=True) #removes $ symbol and commas in price string
+        .replace({r"[\$,]"}, '', regex=True) #removes $ symbol and commas in price string
         .astype(float)
     )
 
@@ -49,13 +49,21 @@ def clean_data(df):
     #create a new column for price per bedroom
     df["price_per_room"] = df["price"] / df["bedrooms"]
 
-    print(f"Cleaned data contains {len(df)} rows.")
+    print(f"Cleaned data contains {len(df)} rows and {len(df.columns)} columns.")
     return df
 
 def save_data(df):
     print(f"Saving clean data into {processed_data}...")
     df.to_csv(processed_data, index=False)
     print(f"Saved cleaned data into {processed_data}.")
+
+def run_etl():
+
+    df_raw = load_data(raw_data) #loading data to df
+    df_cleaned = clean_data(df_raw) #cleaning data
+    save_data(df_cleaned) #writing to csv
+
+run_etl()
 
 
 
