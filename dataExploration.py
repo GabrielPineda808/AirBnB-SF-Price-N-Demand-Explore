f5b80dcc8952d0ca6e314c2ef78ec4d82d93eb1f
Gabe Pineda <gabepineda6@gmail.com>
2025-07-07 17:11:19 -0700

    created a bar graph averaging the cost per room type

--- dataExploration.py
+++ dataExploration.py
@@ -16,3 +16,14 @@ plt.title("Price Distribution") #adding title to plot figure
 plt.xlabel("Price ($)")
 plt.ylabel("Count")
 plt.show()
+
+#Average Price by Room Type
+
+room_price =df.groupby("room_type")["price"].mean().sort_values() #grabbing the average grouped by room and sorted by price ascending
+
+room_price.plot(kind="barh", title="Average Price by Room Type") #horizontal bar graph showing price per room type
+
+plt.xlabel("Average Price ($)")
+plt.tight_layout()
+plt.show()
+
